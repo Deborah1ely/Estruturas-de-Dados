bc7fdc34c48dcb38613c5ff76f42bd04f8b9121d
Deborah1ely <deborah.ely@estudante.uffs.edu.br>
2026-03-12 22:07:48 -0300

    Create vetores.c

---
+++ ED/vetores.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const*argv[]){
+    int vetor[5];
+    int i;
+
+    for(int i = 0; i < 5; i++){
+        printf("Digite um número: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("\nVetores: ");
+    for(i = 0; i < 5; i++){
+        printf("%d", vetor[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
